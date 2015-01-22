298fe688904579eba987be4535c1ca44fbf67531
abougeniere <abougeniere@yahoo.fr>
2015-01-22 17:05:21 +0100

    2eme commit

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 /**
- * Created by abougeniere on 21/01/2015.
+ * Created by abougeniere on 22/01/2015.
  */
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+function Counter() {
+    var self = this;
+
+    EventEmitter.call(this); // call EventEmitter constructor
+    var count = 0;
+
+    this.start = function() {
+        this.emit('start');
+
+        setInterval(function() {
+            self.emit('count', count);
+            ++count;
+        }, 1000);
+    };
+}
+
+util.inherits(Counter, EventEmitter); // setup inheritance
+
+
+var counter = new Counter();
+
+counter.once('start', function () {
+    console.log('start event');
+});
+
+counter.on('count', function(count) {
+    console.log('count = ' + count);
+});
+
+counter.start();
